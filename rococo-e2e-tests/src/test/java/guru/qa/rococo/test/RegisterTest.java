d6ad0e27c15887c9df85819cfc30506c9ad237b5
makulich <kulichenkom@ya.ru>
2023-12-05 17:54:34 +0300

    readme update

--- rococo-e2e-tests/src/test/java/guru/qa/rococo/test/RegisterTest.java
+++ rococo-e2e-tests/src/test/java/guru/qa/rococo/test/RegisterTest.java
@@ -1,8 +1,6 @@
 package guru.qa.rococo.test;
 
 import com.codeborne.selenide.Selenide;
-import guru.qa.rococo.jupiter.annotations.ApiLogin;
-import guru.qa.rococo.jupiter.annotations.GenerateUser;
 import guru.qa.rococo.page.MainPage;
 import guru.qa.rococo.page.RegisterPage;
 import io.qameta.allure.AllureId;
@@ -44,17 +42,5 @@ public class RegisterTest extends BaseWebTest {
                 .setPasswordSubmit(secondBasePassword)
                 .errorSubmit()
                 .checkErrorMessage("Passwords should be equal");
-    }
-
-    @Test
-    @ApiLogin(user = @GenerateUser())
-    @AllureId("153")
-    @DisplayName("[WEB] [Registration] User should login after registration")
-    void userShouldLoginAfterRsdaegistering() {
-        String username = generateRandomUsername();
-        Selenide.open(CFG.baseUrl(), MainPage.class)
-                .waitForPageLoaded()
-                .avatarShouldBeVisibleAfterLogin();
-        Selenide.sleep(10000);
     }
 }
